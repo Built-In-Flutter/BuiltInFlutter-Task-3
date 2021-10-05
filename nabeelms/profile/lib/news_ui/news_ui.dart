c58601884b94ec7224c2bec1e52acdb8e4bb206d
Nabeel M.S <nabeel.ms78@gmail.com>
2021-10-05 23:57:06 +0530

    added news home card feature

--- nabeelms/profile/lib/news_ui/news_ui.dart
+++ nabeelms/profile/lib/news_ui/news_ui.dart
@@ -88,39 +88,165 @@ class NewsHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    "Popular",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    "Trending",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: null,
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    "Recent",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: null,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 350,
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    NewsCard(),
+                    NewsCard(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NewsCard extends StatelessWidget {
+  const NewsCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20, left: 15),
+      width: 330,
+      height: 360,
+      decoration: BoxDecoration(
+          border: Border.all(
+            width: .5,
+            color: Colors.grey.shade400,
+          ),
+          borderRadius: BorderRadius.circular(20)),
       child: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.only(right: 30,left: 30),
+            padding: const EdgeInsets.all(20.0),
             child: Row(
-mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
               children: [
-                GestureDetector(
-                  onTap: (){},
-                  child: Text("Popular",style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),),
+                MyBullet(),
+                SizedBox(
+                  width: 10,
                 ),
-                GestureDetector(
-                  onTap: (){},
-                  child: Text("Trending",style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: null,
-                  ),),
+                Text(
+                  "Architecture",
+                  style: TextStyle(fontSize: 18),
+                )
+              ],
+            ),
+          ),
+          Container(
+            width: 300,
+            height: 180,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+              child: Image.network(
+                "https://images.unsplash.com/photo-1613127935403-5456f69bf836?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+                fit: BoxFit.cover,
+                width: 300,
+                height: 180,
+              ),
+            ),
+          ),
+          Container(
+            width: 300,
+            child: Text(
+              "When it comes to design, your eyes are way better than math.",
+              style: TextStyle(fontSize: 22),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: 300,
+            child: Row(
+              children: [
+                Text(
+                  "2 hours ago",
+                  style: TextStyle(color: Colors.grey.shade700),
                 ),
-                GestureDetector(
-                  onTap: (){},
-                  child: Text("Recent",style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: null,
-                  ),),
+                SizedBox(
+                  width: 20,
+                ),
+                MyBullet(),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "3 min read",
+                  style: TextStyle(color: Colors.grey.shade700),
                 ),
               ],
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class MyBullet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      height: 5.0,
+      width: 5.0,
+      decoration: new BoxDecoration(
+        color: Colors.grey,
+        shape: BoxShape.circle,
       ),
     );
   }
