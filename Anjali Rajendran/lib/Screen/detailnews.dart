f382ae01c6cea3c8026c2319f6b68176a446d1f1
Anjali Rajendran <84264801+AnjaliRaj015@users.noreply.github.com>
2021-11-18 21:02:17 +0530

    final commit

--- Anjali Rajendran/lib/Screen/detailnews.dart
+++ Anjali Rajendran/lib/Screen/detailnews.dart
@@ -7,14 +7,14 @@ class DetailNews extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.black,
         elevation: 0,
         actions: [
           Padding(
             padding: const EdgeInsets.all(15.0),
             child: Icon(
               Icons.share,
-              color: Colors.black,
+              color: Colors.deepOrange,
             ),
           ),
         ],
@@ -24,7 +24,7 @@ class DetailNews extends StatelessWidget {
             },
             icon: Icon(
               Icons.arrow_back_ios_new,
-              color: Colors.black,
+              color: Colors.deepOrange,
             )),
       ),
       body: SafeArea(
@@ -39,13 +39,14 @@ class DetailNews extends StatelessWidget {
                     height: 10,
                   ),
                   Text(
-                    "Dassault Rafale (French pronunciation: ​[ʁafal], literally meaning gust of wind, and burst of fire in a more military sense) is a French twin-engine, canard delta wing, multirole fighter aircraft designed and built by Dassault Aviation. Equipped with a wide range of weapons, the Rafale is intended to perform air supremacy, interdiction, aerial reconnaissance, ground support, in-depth strike, anti-ship strike and nuclear deterrence missions. The Rafale is referred to as an omnirole aircraft by Dassault. Introduced in 2001, the Rafale is being produced for both the French Air Force and for carrier-based operations in the French Navy. The Rafale has been marketed for export to several countries, and was selected for purchase by the Egyptian Air Force, the Indian Air Force, the Qatar Air Force, the Hellenic Air Force, and the Croatian Air Force. The Rafale has been used in combat over Afghanistan, Libya, Mali, Iraq and Syria.",
-                    style: TextStyle(fontSize: 16),
+                    "“At the time of Independence, we had about 15 lakh water bodies spread across rural India, but since then, over 12 lakh have been either encroached or polluted,” he said. The impact is being felt now with about 72% of aquifers drying up.He pointed out that the world was heading towards disaster, as water could be the main reason for the beginning of the third World War.“There is forced migration due to shortage of water from Central and western Asia and from Africa. The migrants are moving towards Europe and this is will lead to a major conflict,” said Mr. Rajendra Singh.Migration from India is also heavy, but so far this has mainly been voluntary or economic related.Indians are welcomed as they are not treated as climatic refugees. But days are not far when they will not be given preferential treatment, he said.“Till recently we have been associating global warming with gases, but now, the focus has shifted towards water,” he said.“Due to lack of greenery, the climate has become erratic and the Indian farmers are unable to link up crop pattern with the rainfall pattern.”Elaborating on his campaign ‘water is climate and climate is water,’ he said: “It revolves around six ‘R’s — respect for water, reduce usage, retreat, recycle, recharge and rejuvenate.”If a dry district such as Alwar in Rajasthan can recharge its ground water, why can’t the others, he asked.",
+                      style: TextStyle(fontSize: 16,color: Colors.deepOrange),
                   ),
                 ],
               ),
             ),
-          )),
+          )),        backgroundColor: Colors.black,
+
     );
   }
 }
